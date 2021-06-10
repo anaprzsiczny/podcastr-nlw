2d11a7d306e40ab5573156fe4b2f0b09eb78a2e4
anaprzsiczny <ana.przsiczny@gmail.com>
2021-06-10 18:36:34 -0300

    context to play episode

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,13 @@
+import Image from 'next/image';
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
 export default function Player() {
+
+  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+
+  const episode = episodeList[currentEpisodeIndex]
 
   return (
     <div className={styles.playerContainer}>
@@ -9,9 +16,22 @@ export default function Player() {
         <strong>Tocando agora</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um Podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image 
+            width={592} 
+            height={592} 
+            src={episode.thumbnail} 
+            objectFit="cover" 
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um Podcast para ouvir</strong>
+        </div>
+      )}
 
       <footer className={styles.empty}>
         <div className={styles.progress}>
